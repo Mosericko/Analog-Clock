ffea586311f8ed8d8ccbc550f7ddd0fbfce90a43
Mosericko <mosericko@gmail.com>
2022-05-03 16:27:35 +0300

    Vanilla JS Clock

---
+++ index.js
@@ -0,0 +1,24 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector("[data-hour]");
+const minuteHand = document.querySelector("[data-minute]");
+const secondHand = document.querySelector("[data-second]");
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+  //set the rotations
+
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
